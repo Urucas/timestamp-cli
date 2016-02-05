c110efdee00ea550d94eba81c3decf06ebf39b30
vrunoa <vruno.alassia@gmail.com>
2016-02-05 02:41:10 -0300

    add cli + test

---
+++ test.js
@@ -0,0 +1,20 @@
+var logger = require("semafor")()
+var child = require("child_process")
+var spawn = child.spawnSync("./cli.js")
+var err = spawn.stderr+""
+if(err!= "") {
+  logger.fail(err)
+  process.exit(1)
+}
+var output = (spawn.stdout+"").trim()
+console.log(output)
+if(!/[\d+]/.test(output)) {
+  logger.fail("Invalid timestamp created, "+output)
+  process.exit(1)
+}
+var date = new Date(parseInt(output))
+if(isNaN(date.getTime())){
+  logger.fail("Invalid timestamp created, "+output)
+  process.exit(1)
+}
+process.exit(0)
